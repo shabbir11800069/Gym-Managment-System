fddedc0981d03be4096291fc1af5bced2eb21dc6
Shabbir Ahmed <shabbirahmed11800069@gmail.com>
2022-10-19 10:44:03 +0600

    Add files via upload

---
+++ IDGenerator.py
@@ -0,0 +1,23 @@
+class IDGenerator:    
+    def generateCustomerID():
+        f = open("customer_id", "r+")
+        id = f.readline()
+        id = id.strip()
+        idw = int(id)
+        idw += 1
+        f.seek(0)
+        f.write(str(idw) + "\n")
+        f.truncate()
+        f.close()
+        return int(id)
+    def generatePackageID():
+        f = open("package_id", "r+")
+        id = f.readline()
+        id = id.strip()
+        idw = int(id)
+        idw += 1
+        f.seek(0)
+        f.write(str(idw) + "\n")
+        f.truncate()
+        f.close()
+        return int(id)
